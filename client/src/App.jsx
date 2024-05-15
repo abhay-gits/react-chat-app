414293cf139c28607b08c279c5eb31f25ba2e0ac
Abhay Rana <anabhay42@gmail.com>
2024-05-15 12:57:53 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import socketIO from 'socket.io-client';
 import Home from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ChatPage from './components/ChatPage';
-const socket = socketIO.connect(/* 'https://react-chat-app-wppd.onrender.com', */'http://localhost:4000/');
+const socket = socketIO.connect('https://react-chat-app-wppd.onrender.com');
 
 function App() {
   return (
